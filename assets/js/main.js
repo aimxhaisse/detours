f0a2d72a784ff21a0ba0c587d763183889be8051
mxs <mxs@sbrk.org>
2020-03-22 18:08:06 +0100

    feat: audio player for detours

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,33 @@ $(document).ready(function () {
 	});
     };
 
+    // Audio player.
+    //
+    // - Each link with the "audio-play" class is a player,
+    // - It wraps an audio SVG icon,
+    // - Target link is the audio file to be played,
+    // - Multiple players supported, they need to point to != targets.
     var setUpAudio = function () {
-	$(".audio-play").click(function () {
-	    return false;
+	var audio_preload = {};
+
+	$(".audio-play").each(function () {
+	    var target = $(this).attr("href");
+
+	    audio_preload[target] = new Audio(target);
+	    audio_preload[target].loop = true;
 	});
 
-	$('.audio-icon').css({'fill': "#ffffff"});
+	$(".audio-play").click(function () {
+	    var target = $(this).attr("href");
+
+	    if (audio_preload[target].paused) {
+		audio_preload[target].play();
+	    } else {
+		audio_preload[target].pause();
+	    }
+
+	    return false;
+	});
     };
 
     setUpLightense();
